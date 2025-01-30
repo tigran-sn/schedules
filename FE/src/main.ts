067bf0312211c4ba608232c4fecd0d0ee81f27a9
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2025-01-30 08:00:45 +0400

    Removed commented code

--- FE/src/main.ts
+++ FE/src/main.ts
@@ -1,21 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {
-  HTTP_INTERCEPTORS,
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
-import { routes } from '../src/app/app.routes';
-import { provideRouter } from '@angular/router';
-
-// bootstrapApplication(AppComponent, {
-//   providers: [
-//     provideRouter(routes),
-//     provideHttpClient(withInterceptorsFromDi()), // Enable interceptors
-//     { provide: HTTP_INTERCEPTORS, useClass: ScheduleInterceptor, multi: true },
-//   ],
-// });
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
