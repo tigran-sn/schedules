75d9473dd7f67405e053f8226a8440ca67dac851
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2025-02-02 10:50:55 +0400

    changed DaysOfWeek to English

--- FE/src/app/enums/day-of-week.enum.ts
+++ FE/src/app/enums/day-of-week.enum.ts
@@ -1,9 +1,9 @@
 export enum DayOfWeek {
-  Понедельник = 0,
-  Вторник = 1,
-  Среда = 2,
-  Четверг = 3,
-  Пятница = 4,
-  Суббота = 5,
-  Воскресенье = 6,
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
 }
