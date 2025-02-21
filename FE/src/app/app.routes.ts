8cb6af3273a62910cb05aa711c9a7b9266930df6
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2025-02-21 15:09:21 +0400

    Created unauthorized component and updated the routing

--- FE/src/app/app.routes.ts
+++ FE/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { ScheduleComponent } from './components/schedule/schedule.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { NotFound } from './components/not-found/not-found.component';
+import { RoleGuard } from './guards/role.guard';
+import { Roles } from './enums';
+import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -17,12 +20,18 @@ export const routes: Routes = [
   {
     path: 'users',
     component: UserListComponent,
-    canActivate: [AuthGuard],
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: [Roles.ADMIN] },
   },
   {
     path: 'schedule',
     component: ScheduleComponent,
-    canActivate: [AuthGuard],
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: [Roles.GENERAL, Roles.ADMIN] },
+  },
+  {
+    path: 'unauthorized',
+    component: UnauthorizedComponent,
   },
   { path: '**', component: NotFound },
 ];
